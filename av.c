96bd2a0332786f19bfd28c3aee2afb60ab2fe712
Martin Kielhorn <kielhorn.martin@googlemail.com>
2012-10-18 23:21:50 +0200

    initialize context to NULL before avformat_open_input

--- av.c
+++ av.c
@@ -65,17 +65,19 @@ int vid_init(Vid*v,const char*fn)
 {
   static pthread_mutex_t m=PTHREAD_MUTEX_INITIALIZER;
   pthread_mutex_lock(&m);
-
+  v->ic=NULL;
   if(avformat_open_input(&(v->ic),fn,NULL,NULL)<0)
     return -1;
   if(avformat_find_stream_info(v->ic,NULL)<0)
     return -2;
-
   v->vstream=av_find_best_stream(v->ic,AVMEDIA_TYPE_VIDEO,-1,-1,NULL,0);
+  if(v->vstream<0)
+    return -3;
   v->avctx=v->ic->streams[v->vstream]->codec;
   v->codec = avcodec_find_decoder(v->avctx->codec_id);
+  if(v->codec==0)
+    return -4;
   avcodec_open2(v->avctx,v->codec,NULL);
-
   v->frame=avcodec_alloc_frame();
     
   int w=v->avctx->width,h=v->avctx->height;
@@ -139,8 +141,9 @@ int main()
   Vid*v=vid_alloc();
   vid_init(v,fn);
   Vid*v2=vid_alloc();
-  //vid_init(v2,"/home/martin/Downloads2/RC_helicopter_upside_down_head_touch-1Lg6wASg76o.mp4");
-    
+
+  vid_init(v2,"/home/martin/Downloads2/RC_helicopter_upside_down_head_touch-1Lg6wASg76o.mp4");
+	 
   while(vid_decode_frame(v)){
     static int i=0;
     printf("%d\n",i++);
